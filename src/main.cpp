40c95532cf507307c5204766184a8f1f2551bf45
iamzhaihy <harvey.zhai@gmail.com>
2020-02-19 17:56:02 -0500

    switch to GLM and GLAD

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,52 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void) {
-    GLFWwindow* window;
+#include <iostream>
 
-    /* Initialize the library */
+void mouse_callback(GLFWwindow* window, double xpos, double ypos){};
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){};
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){};
+
+int main(void) {
+    // initialize GLFW
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // use OpenGL 3.3 Core
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
+    // make the window's context current
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    // register callback functions
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
-        /* Render here */
+        // render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
+        // swap front and back buffers
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
+        // Poll for and process events
         glfwPollEvents();
     }
 
